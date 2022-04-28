74f236b4fffc3ba4fff92498b8adad12739f904e
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-28 14:15:15 +0200

    jsnack 2

---
+++ jsnack 2/script.js
@@ -0,0 +1,86 @@
+console.log("test")
+// Jsnack 2
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+const zucchine = [
+    {
+        varieta: "nera",
+        pesoGrammi: 243,
+        lunghezza: [8, "cm"]
+    },
+    {
+        varieta: "romanesco",
+        pesoGrammi: 263,
+        lunghezza: [23, "cm"]
+    },
+    {
+        varieta: "fiorentino",
+        pesoGrammi: 213,
+        lunghezza: [51, "cm"]
+    },
+    {
+        varieta: "napoletana",
+        pesoGrammi: 143,
+        lunghezza: [11, "cm"]
+    },
+    {
+        varieta: "tonde",
+        pesoGrammi: 183,
+        lunghezza: [42, "cm"]
+    },
+    {
+        varieta: "trombetta",
+        pesoGrammi: 278,
+        lunghezza: [6, "cm"]
+    },
+    {
+        varieta: "Crookneck",
+        pesoGrammi: 118,
+        lunghezza: [8.5, "cm"]
+    },
+    {
+        varieta: "pâtisson",
+        pesoGrammi: 207,
+        lunghezza: [10, "cm"]
+    },
+    {
+        varieta: "Trombetta d'Albenga",
+        pesoGrammi: 167,
+        lunghezza: [16, "cm"]
+    },
+    {
+        varieta: "Golden",
+        pesoGrammi: 167,
+        lunghezza: [18, "cm"]
+    }
+]
+
+function separateXWeight(){
+    const over15 = []
+    const under15 = []
+    const weightSeparetor = (x,peso) => (peso < 15) ? under15.push(x) : over15.push(x);
+
+    for(let x of zucchine){
+        console.log(x.lunghezza[0])
+        weightSeparetor(x,x.lunghezza[0])
+        
+    }
+    console.log(over15, "over")
+    console.log(under15, "under")
+    for(let xx of over15){
+        console.log(xx.varieta)
+        writeHtml(".over",xx)
+    }
+    for(let xxx of under15){
+        writeHtml(".under", xxx)
+    }
+}
+
+function writeHtml(ul,x){
+    document.querySelector(ul).innerHTML += `<li>${x.varieta} con ${x.lunghezza[0]}cm  </li>`
+}
+
+separateXWeight()
+
+console.log(separateXWeight())
+// console.log(under15, "under")
